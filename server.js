43714ce8e9b5fa7b8b3c4c209c3af9d873439f66
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-29 12:04:30 -0400

    wip: we can now render the superheroes name, and powers on the DOM

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const superheroes = ['Superman', 'Wonder Woman', 'Black Panther'];
+const superheroes = [
+  {
+    name:'Superman',
+    powers: ['flight', 'invulnerability', 'x-ray vision']
+  },
+  {
+    name: 'Wonder Woman',
+    powers: ['flight', 'invulnerability', 'super strength']
+  },
+  {
+    name: 'Black Panther',
+    powers: ['Agility', ' reflexes', 'superhumanly acute']
+  }
+];
 
 //Index
 app.get('/superheroes', (req, res) => {
@@ -12,9 +25,21 @@ app.get('/superheroes', (req, res) => {
 
 //Show 
 app.get('/superheroes/:index', (req, res) => {
-  console.log(superheroes[req.params.index])
-  const superHero = superheroes[req.params.index];
-  res.send(`<h1>${superHero}</h1>`)
+  //Reference variable that captures the superheroes name from the array of objects
+  const superHeroName = superheroes[req.params.index].name;
+  //Reference variable that captures the  superheroes power array
+  const powers = superheroes[req.params.index].powers;
+
+  //loop through the powers array and create a new <li> and place it in the ul in the res.send in order to display powers as a ul.
+  const singlePower = powers.map(power => `<li>${power}</li>`).join('');
+
+  res.send(`
+    <h1>Name: ${superHeroName}</h1>
+    <h3>Powers</h3>
+    <ul>
+      ${singlePower}
+    </ul>
+    `)
 })
 
 
