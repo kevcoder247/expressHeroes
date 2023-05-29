c9347028ebdee4eeb28f3f60b72fef4caa3b1ee5
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-29 15:26:14 -0400

    final commit

--- server.js
+++ server.js
@@ -17,27 +17,68 @@ const superheroes = [
   }
 ];
 
+const villians = [
+  {
+    name: "Lex Luthor",
+    powers: ["super brain"],
+  },
+  {
+    name: "Ares",
+    powers: ["shape shifting", "teleporting"],
+  },
+  {
+    name: "Killmonger",
+    powers: [
+      "ability to fit in Black Panther's suit",
+      "internet agreement that he looks badass",
+    ],
+  },
+]
+
 //Index
-app.get('/superheroes', (req, res) => {
-  res.send(superheroes)
-  console.log(superheroes)
+app.get('/characters', (req, res) => {
+ const characters = {
+  superheroes: superheroes,
+  villians : villians
+ }
+ res.send(characters);
+ //console.log(characters.superheroes[0])
 })
 
 //Show 
-app.get('/superheroes/:index', (req, res) => {
+app.get('/characters/:index', (req, res) => {
   //Reference variable that captures the superheroes name from the array of objects
   const superHeroName = superheroes[req.params.index].name;
+
   //Reference variable that captures the  superheroes power array
-  const powers = superheroes[req.params.index].powers;
+  const superHeroPowers = superheroes[req.params.index].powers;
 
   //loop through the powers array and create a new <li> and place it in the ul in the res.send in order to display powers as a ul.
-  const singlePower = powers.map(power => `<li>${power}</li>`).join('');
+  const singlePower = superHeroPowers.map(power => `<li>${power}</li>`).join('');
+  
+  //Reference variable that captures the villans name from the array of objects
+  const villanName = villians[req.params.index].name;
 
+  //Reference variable that captures the  Villans power array
+   const villanPowers = villians[req.params.index].powers;
+
+   //loop through the powers array and create a new <li> and place it in the ul in the res.send in order to display powers as a ul.
+   const singleVillanPower = villanPowers.map(power => `<li>${power}</li>`).join('');
+
+  
   res.send(`
     <h1>Name: ${superHeroName}</h1>
     <h3>Powers</h3>
     <ul>
       ${singlePower}
+    </ul>
+ 
+    <hr/>
+
+    <h1>Name: ${villanName}</h1>
+    <h3>Powers</h3>
+    <ul>
+      ${singleVillanPower}
     </ul>
     `)
 })
